bff0662668b17453b4ff1ba9133ce10bc5093f9e
panuwit009 <tinsologuitar@gmail.com>
2025-06-13 20:24:21 +0700

    ควยไรอะ

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './Computer-Guy-Meme--Streamline-Memes.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Page1 () {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +20,46 @@ function App() {
         </a>
       </header>
     </div>
-    
+  );
+}
+
+function Page2 () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Page2
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  const [page, setPage] = useState(1);
+
+  let content;
+  if (page === 1) {
+    content = <Page1 />;
+  } else if (page === 2) {
+    content = <Page2 />;
+  }
+
+  return (
+    <>
+      <button onClick={() => setPage(1)}>1</button>
+      <button onClick={() => setPage(2)}>2</button>
+      {content}
+    </>
   );
 }
 
