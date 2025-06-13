c06ac73bdcc2ab480bd1495420790acce9e8aa29
partyend <sugar13055@gmail.com>
2025-06-13 20:02:37 +0700

    alot

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Test from './Page2';
 import reportWebVitals from './reportWebVitals';
 
+function pages(props){
+if (props.page === 1) {
+  <App />
+} else if (props.page === 2) {
+  <Test />
+}
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+    <button onClick={pages} page="1">1</button>
+    <button onClick={pages} page="2">2</button>
+    </>
   </React.StrictMode>
 );
 
