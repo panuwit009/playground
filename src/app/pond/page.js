dcdae686777e3ea00da3e4c7ee87129cd9fdd208
PPJP246 <rockzx007@gmail.com>
2025-08-05 16:59:22 +0700

    วันละนิดจิตแจ่มใส

--- src/app/pond/page.js
+++ src/app/pond/page.js
@@ -1,58 +1,16 @@
-"use client";
-import { useState, useEffect } from "react";
+"use client"
+import { useState } from "react";
+import Test from "./button/page";
 
-export default function Test () {
-    const [text, setText] = useState(0);
-    useEffect(() => {
-        console.log("ทำงาน");
-    }, [] );
+export default function Close () {
+    const [close, setClose ] = useState(false);
 
-    const [component, setComponent] = useState("first"); 
-    function plusOne () {
-        setText(text + 1);
-        setComponent("second");
-    }
-    
-    return(
-    <>
-        <div className="bg-white w-full h-full flex justify-between p-2 ">
-            <div className="content-center flex gap-2">
-                <button className="bg-red-500 p-2 text-center rounded-xl">โฮม</button>
-                <button className="bg-red-500 p-2 ะext-center rounded-xl">หน้าไหน</button>
-                <button className="bg-red-500 p-2 text-center rounded-xl">งง</button>
-            </div>
-
-            <div>
-                <button className="bg-red-500 p-2 text-center">ออกจากระบบ</button>
+    return (
+        <div className="">
+            <Test setClose={setClose}/>
+            <div className="flex justify-center grid items-center mt-2">
+                { close && <div className="bg-white p-4 text-black h-56 w-lg rounded-xl text-center font-bold "> ควย เปิดหาพ่อมึงหรอ</div>}
             </div>
         </div>
-
-        <div className="dark:text-red-500 text-blue-950 bg-white-500 h-12 w-12">ไก่ {text} ตัว</div>
-        <div className="bg-sky-500 w-full h-full"></div>
-        <div className="w-96">w-96</div>
-
-        <div className="grid grid-flow-col grid-rows-3 gap-4">
-            <div className="grid grid-flow-col grid-rows-3 gap-4 bg-green-500 p-2 row-span-3">
-                <div className="row-span-3 flex justify-center items-center bg-white p-2 rounded-xl 
-                transition duration-350 hover:scale-80 hover:bg-sky-500 hover:text-white font-bold text-[100%]">30</div>
-                <div className="col-span-1 text-center bg-white p-2">31</div>
-                <div className="col-span-1 text-center bg-white p-2">32</div>
-                <div className="col-span-1 text-center bg-white p-2">33</div>
-            </div>
-            <button className="bg-green-500 p-2">2</button>
-            <button className="bg-green-500 p-2">3</button>
-            <button className="bg-green-500 p-2">4</button>
-            <button className="bg-green-500 p-2">5</button>
-            <button className="bg-green-500 p-2">6</button>
-            <button className="bg-green-500 p-2">7</button>
-            <button className="bg-green-500 p-2">8</button>
-            <button className="bg-green-500 p-2">9</button>
-            <button className="bg-green-500 p-2">10</button>
-            <button className="bg-green-500 p-2">11</button>
-            <button className="bg-green-500 p-2">12</button>
-        </div>
-
-    </>
-    );
-    
+    )
 }
