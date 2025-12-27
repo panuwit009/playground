8875bcdfa74f00a2e832eae6d3fab9f2b17cd605
panuwit009 <tinsologuitar@gmail.com>
2025-12-27 18:02:20 +0700

    ควยสักกะริน

--- playground.bun/kuy888/src/index.ts
+++ playground.bun/kuy888/src/index.ts
@@ -1,10 +1,16 @@
 import { Elysia } from "elysia";
 import { staticPlugin } from '@elysiajs/static';
 
-const app = new Elysia().use(await staticPlugin({
+const app = new Elysia()
+.use(await staticPlugin({
   assets: "public",
   prefix: '/'
-})).listen(3000);
+}))
+.use(await staticPlugin({
+  assets: "public",
+  prefix: '/asd'
+}))
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
