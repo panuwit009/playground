253a3e93c7cc1643b9ca98bb545c0c85958634f8
Pond <rockzx007@gmail.com>
2025-12-30 18:23:38 +0700

    ไม่ต้องเสือกอะ ว่าทำอะไร

---
+++ playground.bun/pond/public/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [result, setResult] = useState('')
+
+
+    function login() {
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                setResult(data.message)
+            })
+    }
+
+    return (
+        <>
+            <h2>Login</h2>
+            <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username" /><br /><br />
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" /><br /><br />
+            <button onClick={login}>Login</button>
+
+            <p>{result}</p>
+        </>
+    )
+}
+
+const root = createRoot(document.getElementById('root')!)
+root.render(<App />)
